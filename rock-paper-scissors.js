c316fe3c3219b9dda8789432f3ed74c6eec66771
Chris Piric <cpirdev2025@gmail.com>
2025-11-18 17:28:12 -0500

    Finish connecting game logic to ui
    Changed all the <p> elements to labels.
    
    Added a span element inside the labels displaying the score.
    This makes it easier to store and retrieve the score values by separting
    them from the label text.
    
    Renamed the score labels to player-score-label and computer-score-label.
    
    Renamed results-text label to victory status label. Added label to
    results-text-container and named it computer-choice-text to display the
    computer's choice.
    
    Added a label to display the final winner message and a reset button
    that appears at the end of the game.
    
    Adjusted style sheet according to class name changes.
    
    Removed playGame function and moved most of that code into the
    input-buttons event listener.
    
    Added updateScore function to update the score labels every round.
    renamed declareGameWinner to checkFinalWinner.
    
    Added resetGamefunction and added listener to resetButton.
    
    Removed console.log and replaced with adding textContent to appropriate
    labels.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,21 @@
+const ROCK_VALUE = 1;
+const PAPER_VALUE = 2;
+const SCISSORS_VALUE = 3;
+
+const DRAW = 0;
+const PLAYER_WINS = 1;
+const COMPUTER_WINS = 2;
+
+const NUMBER_OF_POINTS_TO_WIN_GAME = 5;
+
+const inputButtons = document.querySelectorAll(".input-button");
+const playerScoreValue = document.querySelector(".player-score-value");
+const computerScoreValue = document.querySelector(".computer-score-value");
+const computerChoiceText = document.querySelector(".computer-choice-text");
+const victoryStatusText = document.querySelector(".victory-status-text");
+const finalWinnerText = document.querySelector(".final-winner-text");
+const resetButton = document.querySelector(".reset-button");
+
 function getComputerChoice() {
   return Math.trunc(Math.random() * 10) % 3 + 1;
 }
@@ -40,21 +58,21 @@ function playRound(playerChoice, computerChoice) {
     }
   }
 
+  updateScore(winner);
   displayRoundWinner(playerChoice, computerChoice, winner);
-  return winner;
 }
 
 function displayRoundWinner(playerChoice, computerChoice, victoryStatus) {
   let playerChoiceAsString = convertChoiceToString(playerChoice);
   let computerChoiceAsString = convertChoiceToString(computerChoice);
 
-  computerChoiceText.textContent = `Computer chose ${computerChoiceAsString}`;
+  computerChoiceText.textContent = `Computer chose ${computerChoiceAsString}.`;
   if (victoryStatus === DRAW) {
     victoryStatusText.textContent = `Draw!`;
   } else if (victoryStatus === PLAYER_WINS) {
     victoryStatusText.textContent = `You win! ${playerChoiceAsString} beats ${computerChoiceAsString}.`;
   } else {
-    victoryStatusText.textContent = `You lose! ${computerChoiceAsString} beats ${playerChoiceAsString}`;
+    victoryStatusText.textContent = `You lose! ${computerChoiceAsString} beats ${playerChoiceAsString}.`;
   }
 }
 
@@ -78,72 +96,59 @@ function convertChoiceToString(choice) {
   return choiceAsString;
 }
 
-function declareGameWinner(playerScore, computerScore) {
-  if (playerScore > computerScore) {
-    console.log("You are the final winner!");
-  } else if (computerScore > playerScore) {
-    console.log("The computer is the final winner!");
-  } else {
-    console.log("No one won!");
+function checkFinalWinner(playerScore, computerScore) {
+  if (playerScore >= NUMBER_OF_POINTS_TO_WIN_GAME) {
+    finalWinnerText.textContent = `You are the final winner!`;
+    resetButton.style.visibility = "visible";
+  } else if (computerScore >= NUMBER_OF_POINTS_TO_WIN_GAME) {
+    finalWinnerText.textContent = `The computer is the final winner!`;
+    resetButton.style.visibility = "visible";
   }
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  let playerChoice;
-  let computerChoice;
-  let roundWinner;
-
-  for (let round = 0; round < 5; round++) {
-    console.log(`Round ${round + 1}`);
-
-    playerChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-
-    console.log(`You chose ${convertChoiceToString(playerChoice)}.`);
-    console.log(`The computer chose ${convertChoiceToString(computerChoice)}.`);
-
-    roundWinner = playRound(playerChoice, computerChoice);
-
-    if (roundWinner === PLAYER_WINS) {
-      humanScore += 1;
-    } else if (roundWinner === COMPUTER_WINS) {
-      computerScore += 1;
-    }
-
-    console.log(`Player Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-    console.log("*************************************\n\n")
+function updateScore(roundWinner) {
+  if (roundWinner === PLAYER_WINS) {
+    let currentScore = Number(playerScoreValue.textContent);
+    currentScore += 1;
+    playerScoreValue.textContent = currentScore;
+  } else if (roundWinner === COMPUTER_WINS) {
+    let currentScore = Number(computerScoreValue.textContent);
+    currentScore += 1;
+    computerScoreValue.textContent = currentScore;
   }
+}
 
-  declareGameWinner(humanScore, computerScore);
+function resetGame() {
+  // Reset scores to 0
+  playerScoreValue.textContent = 0;
+  computerScoreValue.textContent = 0;
+
+  // Clear output
+  computerChoiceText.textContent = "";
+  victoryStatusText.textContent = "";
+  finalWinnerText.textContent = "";
+
+  resetButton.style.visibility = "hidden";
 }
 
 function playerChooses(event) {
+  //Get player and computer choices
   const playerChoice = getPlayerChoice(event.target.className.split(" ")[1]);
   const computerChoice = getComputerChoice();
-  console.log(playRound(playerChoice, computerChoice));
+
+  // Start a round
+  playRound(playerChoice, computerChoice)
+
+  // Check for final winner
+  checkFinalWinner(Number(playerScoreValue.textContent), Number(computerScoreValue.textContent));
 }
 
 /* ************Main Code************ */
-const ROCK_VALUE = 1;
-const PAPER_VALUE = 2;
-const SCISSORS_VALUE = 3;
-
-const DRAW = 0;
-const PLAYER_WINS = 1;
-const COMPUTER_WINS = 2;
-
-const inputButtons = document.querySelectorAll(".input-button");
-const playerScoreText = document.querySelector(".player-score-text");
-const computerScoreText = document.querySelector(".computer-score-text");
-const computerChoiceText = document.querySelector(".computer-choice-text");
-const victoryStatusText = document.querySelector(".victory-status-text");
+resetButton.style.visibility = "hidden";
 
 inputButtons.forEach(button => {
   button.addEventListener('click', playerChooses);
 })
 
+resetButton.addEventListener('click', resetGame);
 
